a7e717c0d0aed65e6a6716d6f19f85f8c3d020f3
Antti Sillanpää <peerlez@gmail.com>
2015-01-27 15:34:24 +0200

    Added API Class and DAO class for Access Token.

---
+++ src/main/java/com/peerlez/authorize/AccessTokenDAO.java
@@ -0,0 +1,73 @@
+package com.peerlez.authorize;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.peerlez.authorize.db.DbQuery;
+
+/**
+ * @author A.Sillanpaa
+ *
+ */
+public class AccessTokenDAO {
+
+	private static final Logger LOG = LoggerFactory
+		.getLogger(AccessTokenDAO.class);
+
+	private static AccessTokenDAO _instance;
+	private static ServletContext _servletContext;
+	private final static String DB_CONN = "dbConnection";
+	private static DbQuery _query;
+
+	private AccessTokenDAO(ServletContext servletContext) {
+		_servletContext = servletContext;
+		_query = new DbQuery((Connection) _servletContext.getAttribute(DB_CONN));
+	}
+
+	/**
+	 * Gets the singleton instance of this class.
+	 *
+	 * @return singleton instance of this class
+	 */
+	public static synchronized AccessTokenDAO instance(ServletContext 
+			servletContext) {
+
+		if (null == _instance) {
+			_instance = new AccessTokenDAO(servletContext);
+		}
+
+		return _instance;
+	}
+	
+	/**
+	 * Deletes from DB the AccessToken matching the given token.
+	 *
+	 * @param accessToken 
+	 * 				the Access Token to delete
+	 *
+	 * @return {@code true} if the AccessToken was deleted; {@code false} if not
+	 *         		found
+	 *
+	 * @throws SQLException 
+	 * 				if any database-related error occurs
+	 */
+	public boolean deleteAccessToken(String accessToken) throws SQLException {
+		
+		ResultSet result = _query.initializeCallable("oauth.deletetoken", 
+				true, null);
+		
+		boolean deleted = false;
+		while (result.next()) {
+	          deleted = result.getBoolean(1);
+		}
+		LOG.info("Acccess Token {} deleted", accessToken);
+
+		return deleted;
+	}
+}
---
+++ src/main/java/com/peerlez/authorize/api/Oauth.java
@@ -0,0 +1,62 @@
+package com.peerlez.authorize.api;
+
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.peerlez.authorize.AccessTokenDAO;
+import com.peerlez.authorize.common.OAuthError;
+import com.peerlez.authorize.common.OAuthErrorConstants;
+import com.peerlez.authorize.exception.OauthBadRequestException;
+
+
+/**
+ * Class provides the Authorization Endpoint as defined in OAuth2
+ * specification.
+ * 
+ * @author A.Sillanpaa
+ *
+ */
+@Path("/oauth2")
+public class Oauth {
+
+	@Context
+	private ServletContext _servletContext;
+
+	/**
+	 * Access Token revocation endpoint as defined by OAuth2.
+	 *
+	 * @param token 
+	 * 				The Access token that the client wants to get revoked
+	 * @throws SQLException 
+	 *
+	 * @HTTP 200 The AccessToken was succesfully revoked(deleted from DB)
+	 *
+	 * @see <a href="http ://tools.ietf.org/html/rfc7009">RFC 7009 OAuth 2.0
+	 *      Token Revocation</a>
+	 */
+	@POST
+	@Path("revoke")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response revoke(@FormParam("token") String accessToken) 
+			throws SQLException {
+
+		if (accessToken == null || accessToken.isEmpty()) {
+			throw new OauthBadRequestException(new OAuthError(
+					OAuthErrorConstants.INVALID_REQUEST,
+					OAuthErrorConstants.TOKEN_NEEDED));
+		}
+
+		AccessTokenDAO.instance(_servletContext)
+			.deleteAccessToken(accessToken);
+
+		return Response.ok().build();
+	}
+}
