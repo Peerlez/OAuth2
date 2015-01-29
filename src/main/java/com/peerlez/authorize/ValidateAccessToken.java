5b511f96ef4cd1170d4c308cb31562f154c5393c
Antti Sillanpää <peerlez@gmail.com>
2015-01-29 22:37:12 +0200

    Added interface for different types of  Access Tokens validation.

---
+++ src/main/java/com/peerlez/authorize/ValidateAccessToken.java
@@ -0,0 +1,64 @@
+package com.peerlez.authorize;
+
+import java.sql.SQLException;
+
+import com.peerlez.authorize.resource.BearerAccessToken;
+import com.peerlez.authorize.resource.JwtAccessToken;
+import com.peerlez.authorize.resource.MacAccessToken;
+
+/**
+ * Interface to validate different types of AccessTokens.
+ * 
+ * @author A.Sillanpaa
+ *
+ */
+public interface ValidateAccessToken {
+
+	/**
+	 * Validate <code>Bearer</code> AccessToken
+	 * 
+	 * @param accessToken
+	 * 				AccessToken which needs to be validated
+	 * 
+	 * @return validated
+	 * 				AccessToken {@link BearerAccessToken}; {@code null} if not 
+	 * 				found
+	 * 
+	 * @throws SQLException
+	 * 				if any database-related error occurs
+	 */
+	BearerAccessToken validateBearerAccessToken(String accessToken) 
+		throws SQLException;
+	
+	/**
+	 * Validate <code>MAC</code> AccessToken
+	 * 
+	 * @param accessToken
+	 * 				AccessToken which needs to be validated
+	 * 
+	 * @return validated
+	 * 				AccessToken {@link MacAccessToken}; {@code null} if not 
+	 * 				found
+	 * 
+	 * @throws SQLException
+	 * 				if any database-related error occurs
+	 */
+	MacAccessToken validateMacAccessToken(String accessToken) 
+			throws SQLException;
+	
+	/**
+	 * Validate <code>JWT</code> AccessToken
+	 * 
+	 * @param accessToken
+	 * 				AccessToken which needs to be validated
+	 * 
+	 * @return validated
+	 * 				AccessToken {@link JwtAccessToken}; {@code null} if not 
+	 * 				found
+	 * 
+	 * @throws SQLException
+	 * 				if any database-related error occurs
+	 */
+	JwtAccessToken validateJwtAccessToken(String accessToken) 
+			throws SQLException;
+}
